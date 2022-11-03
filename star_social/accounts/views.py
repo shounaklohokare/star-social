b6c686ce3fb8292e7107d5dbf56c77c8c52b6c10
Shounak Lohokare <shounaklohokare@gmail.com>
2022-11-03 19:34:31 +0530

    added view and form for user signup

--- star_social/accounts/views.py
+++ star_social/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.core.unresolvers import reverse_lazy
+from django.views.generic import CreateView
 # Create your views here.
+
+from . import forms
+
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    # will redirect to the url after successful sign up
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
