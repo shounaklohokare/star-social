16f52175221915172f549523f1a0a92609c5f457
Shounak Lohokare <shounaklohokare@gmail.com>
2022-11-07 16:53:04 +0530

    registered models and connected views to urls

--- star_social/groups/models.py
+++ star_social/groups/models.py
@@ -1,6 +1,6 @@
 from django import template
 from django.db import models
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
